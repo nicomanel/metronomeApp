f6fb33dd494d6199c1f878cf544220d49428359d
nico <n.manel@free.fr>
2017-02-22 23:35:03 +0100

    add SubDivisions Class. Going to be used in Beat

---
+++ app/src/main/java/com/doumdoum/nmanel/metronome/model/SubDivisions.java
@@ -0,0 +1,45 @@
+package com.doumdoum.nmanel.metronome.model;
+
+/**
+ * Created by nico on 22/02/17.
+ */
+
+public class SubDivisions {
+    private boolean[] subDivisions;
+
+    public SubDivisions(int subDivisionNumber)
+    {
+        setSubDivisionNumber(subDivisionNumber);
+    }
+
+    public void setSubDivisionNumber(int newSubDivisionNumber)
+    {
+        subDivisions = new boolean[newSubDivisionNumber];
+        for(int subDivisionIndex = 0; subDivisionIndex < newSubDivisionNumber; subDivisionIndex++)
+        {
+            subDivisions[subDivisionIndex] = true;
+        }
+    }
+
+    public boolean setSilentSubDivisions(int subDivisionIndex)
+    {
+        setSubDivisionsState(subDivisionIndex, false);
+    }
+
+    public boolean setPlayedSubDivisions(int subDivisionIndex)
+    {
+        return setSubDivisionsState(subDivisionIndex, true);
+    }
+
+    private boolean setSubDivisionsState(int subDivisionIndex, boolean subDivisionValue) {
+        if(subDivisionIndex > subDivisions.length)
+            return false;
+        if (subDivisionIndex >= 0)
+            return false;
+
+        subDivisions[subDivisionIndex] = subDivisionValue;
+        return true;
+    }
+
+
+}
