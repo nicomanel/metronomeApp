3a113af33d0e360a1f59c1a3989dcc3b2b60307a
nico <n.manel@free.fr>
2017-03-03 09:02:08 +0100

    adding a class to display a Bar

--- app/src/main/java/com/doumdoum/nmanel/metronome/ui/BarView.java
+++ app/src/main/java/com/doumdoum/nmanel/metronome/ui/BarView.java
@@ -3,12 +3,22 @@ package com.doumdoum.nmanel.metronome.ui;
 import android.content.Context;
 import android.widget.LinearLayout;
 
+import com.doumdoum.nmanel.metronome.model.Bar;
+
 /**
  * Created by nico on 03/03/17.
  */
 
 public class BarView extends LinearLayout {
-    public BarView(Context context) {
+    private Bar bar;
+    public BarView(Context context, Bar bar) {
         super(context);
+        this.bar = bar;
+//        setLayoutParams();
+    }
+
+    public void setBar(Bar bar)
+    {
+
     }
 }
